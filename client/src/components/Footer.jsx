3de7102630e8a61f35792ce2e2c9432a7df8e1e1
yjj <jyj0225@naver.com>
2024-07-26 11:21:23 +0900

    yj - addition page (0726-1121)

---
+++ client/src/components/Footer.jsx
@@ -0,0 +1,9 @@
+const Footer = () => {
+  return (
+    <footer className="bg-primary text-primary-foreground py-4 px-6 text-center">
+      <p>&copy; 2023 부동산 정보 서비스. All rights reserved.</p>
+    </footer>
+  );
+};
+
+export default Footer;
